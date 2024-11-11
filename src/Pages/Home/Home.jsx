57d405503f1ad1620234f3396e687883cb919b6a
seyam <mdseyam3215@gmail.com>
2024-11-11 13:07:35 +0600

    setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+            Hello Home!
+           </h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element:<Home></Home> ,
+      },
+    ],
+  },
+]);
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
